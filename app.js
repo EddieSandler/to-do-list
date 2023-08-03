bb0d45a1a1c709fbb75ce319909a83dcb7c23342
Eddie Sandler <ed.sandler@gmail.com>
2023-08-03 12:33:40 -0400

    remove button and complete function completed

--- app.js
+++ app.js
@@ -1,73 +1,39 @@
 const form = document.querySelector('#todo-form');
 const taskInput = document.querySelector('input[name="taskname"]');
-
 const resultSection = document.querySelector('#results');
+const taskList = document.querySelector('#tasklist');
 
-form.addEventListener('submit', addTask)
 
-function addTask(e){
-e.preventDefault();
+form.addEventListener('submit', addTask);
+
+function addTask(e) {
+	e.preventDefault();
 	const myTasks = createTask(taskInput.value);
-	resultSection.append(myTasks);
+
 }
 
 
 function createTask(text) {
 	const newTask = document.createElement('li');
 	newTask.innerText = text;
-	const taskList=document.querySelector('#tasklist')
-	taskList.appendChild(newTask)
-
-		const removeButton = document.createElement("button");
+	taskList.appendChild(newTask);
+	const removeButton = document.createElement("button");
 	removeButton.innerText = "Remove";
-newTask.append(removeButton)
-
+	newTask.append(removeButton);
 	removeButton.addEventListener("click", clearTask);
-return newTask
-
+	newTask.addEventListener('click',completeTask)
+	return newTask;
 }
- function clearTask(e) {
+
+function clearTask(e) {
 	e.preventDefault();
+	e.target.parentElement.remove();
 
-		e.target.parentElement.remove()
-
-	};
-
-
-	// const completedButton = document.createElement("button");
-	// completedButton.innerText = "Mark Completed";
-
-	// newTask.append(removeButton)
-	// newTask.append(completedButton)
-
-
-
-
-//possibly add li for each task
-//append remove and completed buttons to li
-//click handler to remove element
-//click handler to strike through completed tasks
-
-//save task list to local storage
-
-
-// const form = document.querySelector("form");
-// const friendList = document.querySelector("#friend-list");
-
-// form.addEventListener("submit", function(event) {
-//   event.preventDefault();
-//   const newFriendInput = document.querySelector("#first-name");
-//   const newLi = document.createElement("li");
-//   const newButton = document.createElement("button");
-//   newLi.innerText = newFriendInput.value;
-//   newButton.innerText = "Remove";
-
-
-
-//   newLi.append(newButton);
-//   friendList.append(newLi);
-//   form.reset();
-// });
+};
+function completeTask(e){
+	console.log(e.target)
+	e.target.classList.add("completed");
+}
 
 
 
